f73464e1dd225175a25805a5e9017b087a503c80
Ousssama Driouech <thedriwsh@gmail.com>
2026-03-27 14:55:20 +0100

    feat: make_view function  - add options for linking views to routes/controllers via make_view()

--- src/core/helpers.php
+++ src/core/helpers.php
@@ -2,10 +2,14 @@
 
  
     if (!function_exists('make_view')) {
-        function make_view(string $view) {
+        function make_view(string $view , ?array $context = []) {
             if (!is_int($view)) {
-                $view_file = BASE_PATH . "/views/". $view . ".view.php";
-                if (file_exists($view_file)) {
+                if (str_ends_with($view , ".view.php")) {
+                    $view_file = BASE_PATH . "/views/". trim(substr($view , 0 , -9)) . ".view.php";
+                    require_once $view_file;
+                }
+                else if (!str_ends_with($view , ".view.php")) {
+                    $view_file = BASE_PATH . "/views/". trim($view) . ".view.php";
                     require_once $view_file;
                 }
             }
